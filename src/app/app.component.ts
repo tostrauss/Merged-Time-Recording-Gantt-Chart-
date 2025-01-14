395cfdb11fa3a645d7651eecb254fedb9c9000c3
tostrauss <tobias.p.strauss@gmail.com>
2025-01-14 20:23:19 +0100

    TakeAction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { BehaviorSubject, Subscription } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { filter, take } from 'rxjs/operators';
+
 import { GlobalInitializer, AuthGuard } from 'my-library';
 import { GanttChartComponent } from './gantt-chart/gantt-chart.component';
 // Globals
-import { HeaderComponent} from './gantt-chart/header/header.component';
+import { HeaderComponent } from './gantt-chart/header/header.component';
 import { checkmarkOutline, personOutline, settingsOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import path from 'path';
+import { error } from 'console';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   private loadingSubject = new BehaviorSubject<boolean>(false);
   loading$ = this.loadingSubject.asObservable();
   RENDER: boolean = false;
+  globalConfig: any;
+  private globalSubscription!: Subscription;
   ShowHeaderFooter: boolean = true;
 
   constructor(
     private globalInitializer: GlobalInitializer,
     private router: Router,
+    private myLibrary: MyLibraryService  // <-- Using 'MyLibraryService'
   ) {
     // Dynamic Icons * can probably initialize the entire app here 
     addIcons({
@@ -32,18 +36,15 @@ export class AppComponent implements OnInit {
       personOutline,
       checkmarkOutline
     });
-   }
-
-  globalConfig: any;
-  private globalSubscription!: Subscription;
+  }
 
   ngOnInit() {
     // ROUTING HANDLER
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe(() => {
-      window.scrollTo(0, 0);
-    });
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        window.scrollTo(0, 0);
+      });
 
     // Paths 
     /*
@@ -57,32 +58,128 @@ export class AppComponent implements OnInit {
 
     // run into function PathsArray
     // GLOBAL INITIALIZER
-    this.globalSubscription = this.globalInitializer.initGlobals(GLOBALS_App,RoutingArray, 'https://dev.collegerecruit.us/CORE_FE/GATEWAY.php').subscribe((config: any) => {
-      this.globalConfig = config;
-
-      // Set Header Footer Show
-      config.SETTINGS.NoHeadFoot.some((pattern: any) => {
-        const regex = new RegExp(`^${pattern.replace(/:\w+/g, '[^/]+')}$`);
-        return regex.test(this.router.url);
-      });
-
-      //testing routes
-      if (config.CONFIG.DEBUG === true) {
-        console.log(this.globalInitializer.getRoutes());
-      }
-
-      this.RENDER = true; // Set RENDER to true when config is successfully loaded
-      this.loadingSubject.next(false); // Set loading to false
-    }, (error: any) => {
-      this.RENDER = false; // Set RENDER to false if there is an error
-      this.loadingSubject.next(false); // Set loading to false
-    });
+    this.globalSubscription = this.globalInitializer
+      .initGlobals(GLOBALS_App, RoutingArray, 'https://dev.collegerecruit.us/CORE_FE/GATEWAY.php')
+      .subscribe(
+        (config: any) => {
+          this.globalConfig = config;
+          this.myLibrary.setGlobalConfig(config);
+          this.myLibrary.setGlobalInitializer(this.globalInitializer);
+          this.myLibrary.setRouter(this.router);
+          this.RENDER = true;
+        },
+        (error: any) => {
+          console.error(error);
+          this.RENDER = false;
+        }
+      );
   }
 
   ngOnDestroy() {
     if (this.globalSubscription) {
       this.globalSubscription.unsubscribe();
     }
+  }
+
+  // --------------------------------------------------------------------------------
+  // BELOW ARE TAKEACTION EXAMPLES
+  // --------------------------------------------------------------------------------
+
+  /**
+   * Example: Retrieve Time Clock Data
+   */
+  retrieveTimeClockData() {
+    this.myLibrary.TakeAction({ "CALL": 'g3tT1m3' }, true)
+      .pipe(take(1))
+      .subscribe({
+        next: (response: any) => {
+          console.log('Time Clock Data:', response);
+        },
+        error: (err: any) => {
+          console.error('Error retrieving time clock data:', err);
+        }
+      });
+  }
+
+  /**
+   * Example: Retrieve Gantt Data
+   */
+  retrieveGanttData() {
+    this.myLibrary.TakeAction({ "CALL": 'retrieveGanttData' }, true)
+      .pipe(take(1))
+      .subscribe({
+        next: (response: any) => {
+          console.log('Gantt Data:', response);
+        },
+        error: (err: any) => {
+          console.error('Error retrieving Gantt data:', err);
+        }
+      });
+  }
+
+  /**
+   * Example: Update Gantt Data
+   * You can pass additional fields in `data` which get spread into the "CALL" object
+   */
+  updateGanttData(data: any) {
+    this.myLibrary.TakeAction({ "CALL": 'updateGanttData', ...data }, true)
+      .pipe(take(1))
+      .subscribe({
+        next: (response: any) => {
+          console.log('Update Gantt Data Response:', response);
+        },
+        error: (err: any) => {
+          console.error('Error updating Gantt data:', err);
+        }
+      });
+  }
+
+  /**
+   * Example: Update Time Clock Data
+   */
+  updateTimeClockData(data: any) {
+    this.myLibrary.TakeAction({ "CALL": 'updateTimeClockData', ...data }, true)
+      .pipe(take(1))
+      .subscribe({
+        next: (response: any) => {
+          console.log('Update Time Clock Data Response:', response);
+        },
+        error: (err: any) => {
+          console.error('Error updating time clock data:', err);
+        }
+      });
+  }
+
+  /**
+   * Example: Delete Gantt Data
+   */
+  deleteGanttData(id: string) {
+    this.myLibrary.TakeAction({ "CALL": 'deleteGanttData', id }, true)
+      .pipe(take(1))
+      .subscribe({
+        next: (response: any) => {
+          console.log('Delete Gantt Data Response:', response);
+        },
+        error: (err: any) => {
+          console.error('Error deleting Gantt data:', err);
+        }
+      });
+  }
+
+  /**
+   * Example: Delete Time Clock Data
+   */
+  deleteTimeClockData(id: string) {
+    this.myLibrary.TakeAction({ "CALL": 'deleteTimeClockData', id }, true)
+      .pipe(take(1))
+      .subscribe({
+        next: (response: any) => {
+          console.log('Delete Time Clock Data Response:', response);
+        },
+        error: (err: any) => {
+          console.error('Error deleting time clock data:', err);
+        }
+      });
   }
 }
 
